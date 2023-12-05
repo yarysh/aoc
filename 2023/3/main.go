85bf16dc5f09600119e957c13dcb4877032748ca
Andrey Yarysh <and@yarysh.com>
2023-12-05 21:27:22 +0100

    [2023] 3.2

--- 2023/3/main.go
+++ 2023/3/main.go
@@ -21,6 +21,9 @@ func main() {
 
 	f.Seek(0, 0)
 	fmt.Println("Part 1", part1(f))
+
+	f.Seek(0, 0)
+	fmt.Println("Part 2", part2(f))
 }
 
 func part1(f io.Reader) int {
@@ -82,6 +85,98 @@ func part1(f io.Reader) int {
 	return result
 }
 
+func part2(f io.Reader) int {
+	result := 0
+
+	b, _ := io.ReadAll(f)
+	lines := strings.Split(string(b), "\n")
+
+	top := make([]int, len(lines[0]))
+	curr := l2arr(lines[0])
+	btm := make([]int, len(lines[0]))
+
+	for i := 0; i < len(lines)-1; i++ {
+		btm = l2arr(lines[i+1])
+
+		for j, d := range curr {
+			if d != -1 {
+				continue
+			}
+
+			var nums []int
+
+			nums = append(nums, findNums(top, j)...)
+			nums = append(nums, findNums(curr, j)...)
+			nums = append(nums, findNums(btm, j)...)
+
+			if len(nums) == 2 {
+				result += nums[0] * nums[1]
+			}
+		}
+
+		top, curr = curr[:], btm[:]
+	}
+
+	return result
+}
+
+func l2arr(line string) []int {
+	result := make([]int, len(line))
+
+	for i, ch := range line {
+		if ch >= '0' && ch <= '9' {
+			result[i] = int(ch - '0')
+			continue
+		}
+
+		if ch == '*' {
+			result[i] = -1
+			continue
+		}
+
+		result[i] = -2
+	}
+
+	return result
+}
+
+func findNums(arr []int, pos int) []int {
+	var result []int
+
+	left, right := max(pos-1, 0), min(pos+1, len(arr)-1)
+	for ; left > 0 && arr[left] >= 0; left-- {
+	}
+
+	for ; right < len(arr)-1 && arr[right] >= 0; right++ {
+	}
+
+	var nums [][]int
+	var num []int
+
+	for _, n := range arr[left : right+1] {
+		if n < 0 {
+			if len(num) > 0 {
+				nums = append(nums, num)
+				num = nil
+			}
+			continue
+		}
+
+		num = append(num, n)
+	}
+
+	if len(num) > 0 {
+		nums = append(nums, num)
+		num = nil
+	}
+
+	for _, n := range nums {
+		result = append(result, aToD(n))
+	}
+
+	return result
+}
+
 func parseLine(line string, check func(i int, line string) bool) ([]int, []bool) {
 	var nums = make([]int, len(line))
 	for i, _ := range nums {
@@ -111,30 +206,8 @@ func parseLine(line string, check func(i int, line string) bool) ([]int, []bool)
 func aToD(nums []int) int {
 	result := 0
 
-	start := 0
-	for i, n := range nums {
-		if n != -1 {
-			start = i
-			break
-		}
-	}
-
-	end := len(nums)
 	for i := len(nums) - 1; i >= 0; i-- {
-		if nums[i] != -1 {
-			end = i
-			break
-		}
-	}
-
-	filteredNums := nums[start : end+1]
-
-	for k := len(filteredNums) - 1; k >= 0; k-- {
-		if filteredNums[k] == -1 {
-			return 0
-		}
-
-		result += filteredNums[k] * int(math.Pow10(len(filteredNums)-1-k))
+		result += nums[i] * int(math.Pow10(len(nums)-1-i))
 	}
 
 	return result
